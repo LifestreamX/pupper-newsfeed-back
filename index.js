a48146a69cf9b7b19020c1dcf84daa21b1214f55
haloandfable <tylerallen@live.com>
2022-09-23 12:51:51 -0400

    make it better

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/routes/posts', postRoutes);
+app.use('/posts', postRoutes);
 
-app.get('/', (req, res) => {
+app.get('/posts', (req, res) => {
   res.send('Hello Pupper News Feed Api');
 });
 
